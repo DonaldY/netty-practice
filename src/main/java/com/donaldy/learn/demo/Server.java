ac98bf8e8cdd596f5fb4ff0330ead2e48c43150d
Donald <448641125@qq.com>
2019-09-13 15:52:23 +0800

    add netty demo

---
+++ src/main/java/com/donaldy/learn/demo/Server.java
@@ -0,0 +1,49 @@
+package com.donaldy.learn.demo;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+public class Server {
+
+    public static void main(String[] args) {
+
+        int port = 8000;
+
+        EventLoopGroup bossGroup = new NioEventLoopGroup();
+        EventLoopGroup workGroup = new NioEventLoopGroup();
+
+        try {
+
+            ServerBootstrap b = new ServerBootstrap();
+
+            b.group(bossGroup, workGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel(SocketChannel ch) throws Exception {
+
+                            ch.pipeline().addLast(new ServerHandler());
+                        }
+                    });
+
+            ChannelFuture f = b.bind(port).sync();
+
+            f.channel().closeFuture().sync();
+
+        } catch(Exception e){
+
+            e.printStackTrace();
+
+        } finally {
+
+            bossGroup.shutdownGracefully();
+
+            workGroup.shutdownGracefully();
+        }
+    }
+}
