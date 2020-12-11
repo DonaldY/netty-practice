53046a8b5e7aadb51ceff2e27234cf9d7f699be4
Donald <448641125@qq.com>
2020-12-12 03:05:55 +0800

    socket demo

---
+++ src/main/java/com/donaldy/learn/demo3/HttpServer02.java
@@ -0,0 +1,40 @@
+package com.donaldy.learn.demo3;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ * @author donald
+ * @date 2020/12/12
+ */
+public class HttpServer02 {
+
+    public static void main(String[] args) throws IOException {
+
+        ServerSocket serverSocket = new ServerSocket(8802);
+        while (true) {
+
+            final Socket socket = serverSocket.accept();
+            new Thread(() -> {
+                service(socket);
+            }).start();
+        }
+    }
+
+    private static void service(Socket socket) {
+        try {
+            Thread.sleep(20);
+            PrintWriter printWriter = new PrintWriter(socket.getOutputStream());
+            printWriter.println("HTTP/1.1 200 OK");
+            printWriter.println("Content-Type:text/html;charset=utf-8");
+            printWriter.println();
+            printWriter.write("hello,nio");
+            printWriter.close();
+            socket.close();
+        } catch (InterruptedException | IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
