66ce9eb2919753305ded8ef98a66b44637faf462
Donald <448641125@qq.com>
2021-02-01 15:58:20 +0800

    update timer comment

--- src/main/java/com/donaldy/wheel/HashedWheelTimerTest.java
+++ src/main/java/com/donaldy/wheel/HashedWheelTimerTest.java
@@ -14,13 +14,16 @@ import java.util.concurrent.TimeUnit;
  */
 public class HashedWheelTimerTest {
     public static void main(String[] args) {
+        // 构造一个 Timer 实例
         Timer timer = new HashedWheelTimer();
+        // 提交一个任务，让它在 10s 后执行
         Timeout timeout1 = timer.newTimeout(new TimerTask() {
             @Override
             public void run(Timeout timeout) {
                 System.out.println("timeout1: " + new Date());
             }
         }, 10, TimeUnit.SECONDS);
+        // 取消掉那个 10s 后执行的任务
         if (!timeout1.isExpired()) {
             timeout1.cancel();
         }
