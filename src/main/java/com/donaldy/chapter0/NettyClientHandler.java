9523202dd0c5a3a864dea56648b9cc0ccfa1469c
Donald <448641125@qq.com>
2021-06-21 09:31:23 +0800

    add netty demo

---
+++ src/main/java/com/donaldy/chapter0/NettyClientHandler.java
@@ -0,0 +1,10 @@
+package com.donaldy.chapter0;
+
+import io.netty.channel.ChannelHandlerAdapter;
+
+/**
+ * @author donald
+ * @date 2021/06/21
+ */
+public class NettyClientHandler extends ChannelHandlerAdapter {
+}
